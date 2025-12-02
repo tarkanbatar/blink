c900636db6954af2181a06c95966ed71e619be5c
Tarkan Batar <tarkanbatar4@gmail.com>
2025-12-02 23:21:14 +0300

    project initialization

---
+++ graphql-gateway/src/main/java/com/blink/graphql_gateway/GraphqlGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.blink.graphql_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlGatewayApplication.class, args);
+	}
+
+}
---
+++ notification-service/src/test/java/com/blink/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blink.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
