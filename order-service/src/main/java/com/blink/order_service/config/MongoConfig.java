56c4a8bb815d194139c884e3107e0d1822a616ab
Tarkan Batar <tarkanbatar4@gmail.com>
2025-12-10 16:01:51 +0300

    feat: config

---
+++ order-service/src/main/java/com/blink/order_service/config/MongoConfig.java
@@ -0,0 +1,10 @@
+package com.blink.order_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+    
+}
